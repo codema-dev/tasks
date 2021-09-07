f85ba9ac8b15f6222a082c96f5ef8f85f547db96
Rowan Molony <rowan.molony@codema.ie>
2021-09-07 12:54:45 +0100

    from requests.api import options
    Add overwrite option
    
    Only re-download the file if specified otherwise skip!

--- codema_dev_tasks/requests.py
+++ codema_dev_tasks/requests.py
@@ -10,31 +10,29 @@ from ._compat import import_optional_dependency
 
 URL = Dict[str, Callable[[str, Union[hyperlink.URL, hyperlink.DecodedURL]], None]]
 
-
-def _fetch_from_s3(product: str, url: URL) -> None:
+def _fetch_from_s3(savepath: Path, url: URL) -> None:
 
     fs = import_optional_dependency("fs", "fs is required to run fetch_s3_file")
     from fs.tools import copy_file_data
 
     bucket = url.host
 
-    filepath = Path(product)
-    filename = filepath.name
-    if not filepath.exists():
-        s3fs = fs.open_fs(bucket)
-        with s3fs.open(filename, "rb") as remote_file:
-            with open(filepath, "wb") as local_file:
-                copy_file_data(remote_file, local_file)
+    filename = savepath.name
+    s3fs = fs.open_fs(bucket)
+    with s3fs.open(filename, "rb") as remote_file:
+        with open(savepath, "wb") as local_file:
+            copy_file_data(remote_file, local_file)
 
 
-def _fetch_from_http(product: str, url: URL) -> None:
-    filepath = Path(product)
+def _fetch_from_http(savepath: Path, url: URL) -> None:
     r = requests.get(url)
-    with open(filepath, "wb") as f:
+    with open(savepath, "wb") as f:
         f.write(r.content)
 
 
-def fetch_file(product: str, url: str):
+def fetch_file(product: str, url: str, overwrite: bool = False):
+    savepath = Path(product) 
+
     parsed_url = hyperlink.parse(url)
     _scheme_map = {
         "http": _fetch_from_http,
@@ -42,4 +40,6 @@ def fetch_file(product: str, url: str):
         "s3": _fetch_from_s3,
     }
     fetch = _scheme_map[parsed_url.scheme]
-    fetch(product, parsed_url)
+
+    if not savepath.exists() or overwrite:
+        fetch(savepath, parsed_url)
