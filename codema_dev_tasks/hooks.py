c0d935cbf7c4714317912af2b1f23bd92d74af5d
Rowan Molony <rowan.molony@codema.ie>
2021-09-10 10:09:59 +0100

    Require .env path for load_environmental_variables
    Closes #2

--- codema_dev_tasks/hooks.py
+++ codema_dev_tasks/hooks.py
@@ -1,6 +1,15 @@
+import os
+import sys
+from typing import Union
+
 from ._compat import import_optional_dependency
 
-def load_environmental_variables():
+if sys.version_info >= (3, 6):
+    _PathLike = os.PathLike
+else:
+    _PathLike = str
+
+def load_environmental_variables(dotenv_path: Union[str, _PathLike, None]):
     dotenv = import_optional_dependency("dotenv", "python-dotenv is required to run load_environmental_variables")
     from dotenv import load_dotenv
-    load_dotenv()
+    load_dotenv(dotenv_path=dotenv_path)
