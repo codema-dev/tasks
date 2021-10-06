67fd8b09578c60553865f6a9a925a65c50fc21c1
Rowan Molony <rowan.molony@codema.ie>
2021-10-06 13:50:23 +0100

    Optionally load_dotenv before downloading file
    Enables easier downloading from s3 as can specify within the download
    function to load credentials.
    
    Ploomber supports on_render at task
    or DAG level, however, these implementations bug when the pipeline
    is run via Jupyter Lab raises a DAGRenderError for the DAG level
    hook and task level hook isn't loading credentials...

--- codema_dev_tasks/requests.py
+++ codema_dev_tasks/requests.py
@@ -1,14 +1,33 @@
+from os import PathLike
 from pathlib import Path
-import requests
-from typing import Dict
-from typing import Callable
-from typing import Tuple
-from typing import Union
+from typing import Optional
 
 from ._compat import import_optional_dependency
 
-def fetch_file(product: str, url: str, overwrite: bool = False):
-    fsspec = import_optional_dependency("fsspec", "python-dotenv is required to run fetch_file")
+
+def fetch_file(
+    product: PathLike,
+    url: str,
+    dotenv_path: Optional[PathLike] = None,
+    overwrite: bool = False,
+):
+    """Fetch files via fsspec
+    
+    Args:
+        product (PathLike): Filepath to output
+        url (str): URL to file
+        dotenv_path (Optional[PathLike], optional): Filepath to .env file of
+            environmental variables. Defaults to None.
+        overwrite (bool, optional): Overwrite existing file. Defaults to False.
+    """
+    fsspec = import_optional_dependency(
+        "fsspec", "fsspec is required to run fetch_file"
+    )
+    if dotenv_path:
+        dotenv = import_optional_dependency(
+            "dotenv", "python-dotenv is required to load_dotenv in fetch_file"
+        )
+        dotenv.load_dotenv(dotenv_path=dotenv_path)
     savepath = Path(product)
     file_exists = savepath.exists()
     if (file_exists and overwrite) or (not file_exists):
